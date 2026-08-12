5d4c548c237609bcf2ebaa39c0fdc60acf036d6a
Emmanuel <omidirae@sheridancollege.ca>
2026-08-12 10:06:47 -0400

    Add files via upload

---
+++ Games/Shabd/serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDev = "shabd"
+const service_worker_version = "v1.41"
+const assets = [
+  "/",
+  "index.html",
+  "style.css",
+  "declensions.txt",
+  "script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
